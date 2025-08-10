9f75bfc605562226b3ad09187de09a5554e7c35e
Azeezfasasi <fas.azeez@gmail.com>
2025-08-10 17:23:45 +0100

    coool

--- src/config/Api.jsx
+++ src/config/Api.jsx
@@ -1,2 +1,2 @@
-export const BACKEND_URL = 'https://tofar-logistics-backend.onrender.com';
-export const API_BASE_URL = 'https://tofar-logistics-backend.onrender.com';
+export const BACKEND_URL = 'https://tofar-logistics-backend-lzfo.onrender.com';
+export const API_BASE_URL = 'https://tofar-logistics-backend-lzfo.onrender.com';
