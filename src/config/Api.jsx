da343942b6575d1a4d18e355b94c2508c5b1cf3c
Azeezfasasi <fas.azeez@gmail.com>
2025-08-10 17:00:43 +0100

    Changed API URL

--- src/config/Api.jsx
+++ src/config/Api.jsx
@@ -1,2 +1,2 @@
-export const BACKEND_URL = 'https://cargo-realm-backend.onrender.com';
-export const API_BASE_URL = 'https://cargo-realm-backend.onrender.com/api';
+export const BACKEND_URL = 'https://tofar-logistics-backend.onrender.com';
+export const API_BASE_URL = 'https://tofar-logistics-backend.onrender.com';
